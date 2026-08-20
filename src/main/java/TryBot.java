85a3bf08ca586044db71a56003b2ab61bc7830c9
Ray <rayngzx22@gmail.com>
2026-08-21 02:49:12 +0800

    Add list, Level 2

--- src/main/java/TryBot.java
+++ src/main/java/TryBot.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
 /**
- * A simple chatbot that echoes commands until the user says goodbye.
+ * A simple chatbot that stores tasks until the user says goodbye.
  */
 public class TryBot {
+    private static final int MAX_TASKS = 100;
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String separator = "____________________________________________________________";
         String banner = " _____             ____        _\n"
                 + "|_   _| _ __ _   _ | __ )  ___ | |_\n"
                 + "  | |  | '__| | | ||  _ \\ / _ \\| __|\n"
@@ -13,25 +15,38 @@ public class TryBot {
                 + "  |_|  |_|   \\__, ||____/ \\___/ \\__|\n"
                 + "              |___/\n";
 
-        System.out.println(separator);
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
+
+        System.out.println(SEPARATOR);
         System.out.print(banner);
         System.out.println("Hello! I'm TryBot.");
         System.out.println("What can I do for you?");
-        System.out.println(separator);
+        System.out.println(SEPARATOR);
 
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
-            System.out.println(separator);
+            System.out.println(SEPARATOR);
 
-            if (command.equals("bye") || command.equals("BYE") || command.equals("Bye") || command.equals("bye!") || command.equals("Bye!")) {
+            if (command.equalsIgnoreCase("bye") || command.equalsIgnoreCase("bye!")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(separator);
+                System.out.println(SEPARATOR);
                 break;
             }
 
-            System.out.println(command);
-            System.out.println(separator);
+            if (command.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("Here is your list:");
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("TryBot has added the task: " + command);
+            }
+
+            System.out.println(SEPARATOR);
         }
     }
 }
