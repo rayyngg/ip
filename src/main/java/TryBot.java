300f6ee9285ffb239f6ce004b8e8b43267578548
Ray <rayngzx22@gmail.com>
2026-08-21 02:23:01 +0800

    Updated Banner and changed name to TryBot

---
+++ src/main/java/TryBot.java
@@ -0,0 +1,11 @@
+public class TryBot {
+    public static void main(String[] args) {
+        String banner = " _____             ____        _\n"
+                + "|_   _| _ __ _   _ | __ )  ___ | |_\n"
+                + "  | |  | '__| | | ||  _ \\ / _ \\| __|\n"
+                + "  | |  | |  | |_| || |_) | (_) | |_\n"
+                + "  |_|  |_|   \\__, ||____/ \\___/ \\__|\n"
+                + "              |___/\n";
+        System.out.println(banner);
+    }
+}
